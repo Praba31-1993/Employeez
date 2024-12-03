ed1cea4904e381ebfb988dd68fdf98832fad09d5
Praba31-1993 <prabaidymoto@gmail.com>
2024-12-03 20:59:35 +0530

    anniversary

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/page";
 import ListCard from "../reusableComponent/listitems";
 import ProfilesCard from "./profilescard";
@@ -12,10 +12,31 @@ import ImageIcon from "@mui/icons-material/Image";
 import { Divider } from "@mui/material";
 import { Colors } from "../reusableComponent/styles";
 import ToDoList from "./toDoList";
+import Menulistforbirthdayworkvacation from "../reusableComponent/menulistforbirthdayworkvacation";
+import birthday from "@/app/assets/img/birthday.svg";
+import anniversary from "@/app/assets/img/anniversary.svg";
 
 const Dashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [birthdayAnniversaryReport, setbirthdayAnniversaryReport] = useState();
   const useColors = Colors();
+
+  const arrayList: any = [
+    {
+      name: "Gloria mehckilm",
+      role: "Assistant manager",
+      day: "Today",
+    },
+    {
+      name: "David mechkam",
+      role: "HR",
+      day: "Tomorrow",
+    },
+  ];
+
+  useEffect(() => {
+    setbirthdayAnniversaryReport(arrayList);
+  }, []);
 
   return (
     <Sidebar>
@@ -27,7 +48,7 @@ const Dashboard = () => {
               <ProfilesCard />
             </div>
           </div>
-          {/* List Card Section */}
+
           <div className="col-12 mb-3 col-md-4">
             <div
               className=" dashboardcard  p-3 h-100"
@@ -61,9 +82,87 @@ const Dashboard = () => {
               <ToDoList />
             </div>
           </div>
+          <div className=" col-12 mb-3 col-md-4 ">
+            <div
+              className="dashboardcard p-3 mb-3"
+              style={{
+                border: useColors.border,
+                boxShadow: useColors.boxshadow,
+              }}
+            >
+              <Menulistforbirthdayworkvacation
+                title={"Upcoming birthday"}
+                headerImage={birthday}
+                items={birthdayAnniversaryReport}
+              />
+            </div>
+            <div
+              className="dashboardcard p-3"
+              style={{
+                border: useColors.border,
+                boxShadow: useColors.boxshadow,
+              }}
+            >
+              <Menulistforbirthdayworkvacation
+                title={"Work anniversary"}
+                headerImage={anniversary}
+                items={birthdayAnniversaryReport}
+              />
+            </div>
+          </div>
+          <div className=" col-12 mb-3 col-md-7 ">
+            <div className="row ">
+              <div
+                className="col-sm-12 col-md-5 dashboardcard p-3 h-100 "
+                style={{
+                  border: useColors.border,
+                  boxShadow: useColors.boxshadow,
+                }}
+              >
+                <ToDoList />
+              </div>
+              <div
+                className="col-sm-12 col-md-5 dashboardcard p-3 h-100 ml-5"
+                style={{
+                  border: useColors.border,
+                  boxShadow: useColors.boxshadow,
+                }}
+              >
+                <Menulistforbirthdayworkvacation
+                  title={"Vacation report"}
+                  headerImage={anniversary}
+                  items={birthdayAnniversaryReport}
+                />
+              </div>
+              <div
+                className="col-sm-12 col-md-5 dashboardcard p-3 h-100 ml-5"
+                style={{
+                  border: useColors.border,
+                  boxShadow: useColors.boxshadow,
+                }}
+              >
+                <Menulistforbirthdayworkvacation
+                  title={"Vacation report"}
+                  headerImage={anniversary}
+                  items={birthdayAnniversaryReport}
+                />
+              </div>
+              <div
+                className="col-sm-12 col-md-5 dashboardcard p-3 h-100 ml-5"
+                style={{
+                  border: useColors.border,
+                  boxShadow: useColors.boxshadow,
+                }}
+              >
+                <Menulistforbirthdayworkvacation
+                  title={"Vacation report"}
+                  headerImage={anniversary}
+                  items={birthdayAnniversaryReport}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-     
-       
       </div>
     </Sidebar>
   );
