308219757f6bc066658a7dc25c38b575364e865d
mohamedsafiullah9553 <mohamedsafiullah9553@gmail.com>
2024-12-04 20:55:02 +0530

    dashboard design changed

--- app/reusableComponent/listitems.tsx
+++ app/reusableComponent/listitems.tsx
@@ -3,19 +3,25 @@ import * as React from "react";
 import List from "@mui/material/List";
 import Avatar from "@mui/material/Avatar";
 import ClickableChips from "./chips";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import { Colors } from "./styles";
 
 export default function ListCard() {
+  const useColors = Colors();
   return (
     <List dense sx={{ width: "100%", bgcolor: "background.paper", p: 0 }}>
       <div className="pb-2 textheader heading2" >My Request</div>
 
-      {[0, 1].map((value) => {
+      {[0, 1,2].map((value) => {
         const labelId = `checkbox-list-secondary-label-${value}`;
         return (
-          <div className="d-flex justify-content-between align-items-center pb-2">
+          <div className="d-flex justify-content-between mt-2 align-items-center pb-2">
             <div className="d-flex align-items-center">
-              <Avatar src="" alt="Remy Sharp" />
-              <h5 className="para ps-2 mb-0 ">Your request for leave</h5>
+              <div className="rounded-circle text-white" style={{background:useColors.themeRed}}>
+                <EditNoteIcon className="m-1" />
+              </div>
+              <h5 className="para px-2 mb-0 textheader ellipsis-applied">Your request for leave on 12 Oct 2024
+                has been pending </h5>
             </div>
             <ClickableChips label={"Pending"} />
           </div>
