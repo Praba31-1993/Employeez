5f9a05613236198d32506aa6b93365594add619a
mohamedsafiullah9553 <mohamedsafiullah9553@gmail.com>
2024-12-09 12:39:42 +0530

    dashboard alignment changes updated

--- app/dashboard/components/menulistforbirthdayworkvacation.tsx
+++ app/dashboard/components/menulistforbirthdayworkvacation.tsx
@@ -6,38 +6,38 @@ import user from "@/app/assets/img/Ellipse 14.svg";
 import { Colors } from "../../reusableComponent/styles";
 
 interface MenulistforbirthdayworkvacationProps {
-  headerImage: any;
-  title: string;
-  items: any;
-  isSalesReport?: boolean;
-  lastmonthReport?: string;
+    headerImage: any;
+    title: string;
+    items: any;
+    isSalesReport?: boolean;
+    lastmonthReport?: string;
 }
 
 function Menulistforbirthdayworkvacation({
-  headerImage,
-  title,
-  items,
-  isSalesReport,
-  lastmonthReport,
+    headerImage,
+    title,
+    items,
+    isSalesReport,
+    lastmonthReport,
 }: MenulistforbirthdayworkvacationProps) {
     const useColors = Colors();
     return (
         <div>
-          <div className="d-flex justify-content-between align-items-center">
-            <p className="textheader heading2 mb-0">{title} </p>
-    
-                    {!isSalesReport && (
-                        <Image src={headerImage} alt="" />
-                    )}
-                </div>
-                <div>
-                    {items?.map((bday: any, index:number)  => (
-                        <div key={index}>
+            <div className="d-flex justify-content-between align-items-center">
+                <p className="textheader heading2 mb-0">{title} </p>
+
+                {!isSalesReport && (
+                    <Image src={headerImage} alt="" />
+                )}
+            </div>
+            <div>
+                {items?.map((bday: any, index: number) => (
+                    <div key={index}>
                         <div className="d-flex mt-3 justify-content-between align-items-center pb-2">
                             <div className="d-flex align-items-center">
-                            <div className="userimages">
-                              <Image className="" src={user} alt={""} />
-                              </div>
+                                <div className="userimages">
+                                    <Image className="" src={user} alt={""} />
+                                </div>
                                 <div className="ps-2">
                                     <h5 className="para2 textheader  mb-0 ">{bday?.name}</h5>
                                     <p className="shade para2  mb-0 ">
@@ -45,13 +45,13 @@ function Menulistforbirthdayworkvacation({
                                     </p>
                                 </div>
                             </div>
-                            <p className="para2 mb-0 text-center w-20"  style={{ color: useColors.themeRed }} >{bday?.day}</p>
+                            <p className="para2 mb-0 text-center w-20" style={{ color: useColors.themeRed }} >{bday?.day}</p>
                         </div>
-                        </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-        );
+        </div>
+    );
 }
 
 export default Menulistforbirthdayworkvacation;
