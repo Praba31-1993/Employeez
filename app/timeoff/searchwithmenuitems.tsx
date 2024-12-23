6fa8c85b8023d6dee7163cb704d911bcb83e729b
Praba31-1993 <prabaidymoto@gmail.com>
2024-12-23 12:29:23 +0530

    grid table

--- app/timeoff/searchwithmenuitems.tsx
+++ app/timeoff/searchwithmenuitems.tsx
@@ -15,168 +15,170 @@ import { rows, columns } from "../reusableComponent/JsonData";
 import { SearchLogic } from "../reusableComponent/commonlogic";
 
 function Searchwithmenuitems() {
-    const [ShowColumns, setShowColumns] = useState<boolean>(false);
-    const [tableColumns, setTableColumns] = useState<any>(columns);
-    const [searchList, setSearchList] = useState<any>(columns);
-    const [tableRows, setTableRows] = useState<any>(rows);
-    const [searchQuery, setSearchQuery] = useState<string>("");
-    const [search, setSearch] = useState<string>("");
-    const [allRows, setAllRows] = useState<any>(rows);
-    const columnRef = useRef<HTMLDivElement>(null);
+  const [ShowColumns, setShowColumns] = useState<boolean>(false);
+  const [tableColumns, setTableColumns] = useState<any>(columns);
+  const [searchList, setSearchList] = useState<any>(columns);
+  const [tableRows, setTableRows] = useState<any>(rows);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
+  const [allRows, setAllRows] = useState<any>(rows);
+  const columnRef = useRef<HTMLDivElement>(null);
 
-    const handleChecked = (id: any) => {
-        const updatedColumns = tableColumns.map((columnsList: any) => {
-            if (columnsList.id === id) {
-                return {
-                    ...columnsList,
-                    checked: !columnsList.checked,
-                };
-            }
-            return columnsList;
-        });
-        setTableColumns(updatedColumns);
-        setSearchList(updatedColumns);
+  const handleChecked = (id: any) => {
+    const updatedColumns = tableColumns.map((columnsList: any) => {
+      if (columnsList.id === id) {
+        return {
+          ...columnsList,
+          checked: !columnsList.checked,
+        };
+      }
+      return columnsList;
+    });
+    setTableColumns(updatedColumns);
+    setSearchList(updatedColumns);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        columnRef.current &&
+        !columnRef.current.contains(event.target as Node)
+      ) {
+        setShowColumns(false); // Close the menu if clicking outside
+      }
     };
 
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (
-                columnRef.current &&
-                !columnRef.current.contains(event.target as Node)
-            ) {
-                setShowColumns(false); // Close the menu if clicking outside
-            }
-        };
+    document.addEventListener("mousedown", handleClickOutside); // Listen for clicks
+    return () => document.removeEventListener("mousedown", handleClickOutside); // Cleanup the listener
+  }, []);
 
-        document.addEventListener("mousedown", handleClickOutside); // Listen for clicks
-        return () => document.removeEventListener("mousedown", handleClickOutside); // Cleanup the listener
-    }, []);
+  useEffect(() => {
+    if (search !== "") {
+      const filteredRows = SearchLogic(allRows, search);
+      console.log("Filtered Results:", filteredRows);
+      setTableRows(filteredRows?.length > 0 ? filteredRows : allRows);
+    } else {
+      setTableRows(allRows);
+    }
+  }, [search, allRows]);
 
-    useEffect(() => {
-        if (search !== "") {
-            const filteredRows = SearchLogic(allRows, search);
-            console.log("Filtered Results:", filteredRows);
-            setTableRows(filteredRows?.length > 0 ? filteredRows : allRows);
-        } else {
-            setTableRows(allRows);
-        }
-    }, [search, allRows]);
+  useEffect(() => {
+    if (searchQuery !== "") {
+      const filteredSearchQuery = SearchLogic(columns, searchQuery);
 
-    useEffect(() => {
-        if (searchQuery !== "") {
-            const filteredSearchQuery = SearchLogic(columns, searchQuery);
+      setSearchList(
+        filteredSearchQuery.length > 0 ? filteredSearchQuery : columns
+      );
+    } else {
+      setSearchList(columns);
+    }
+  }, [searchQuery, columns]);
 
-            setSearchList(
-                filteredSearchQuery.length > 0 ? filteredSearchQuery : columns
-            );
-        } else {
-            setSearchList(columns);
-        }
-    }, [searchQuery, columns]);
-
-    return (
-        <>
-            <div className="container-fluid">
-                <div className="">
-                    {ShowColumns && (
-                        <div
-                            className="dashboardcard columnListCard cursorPointer"
-                            ref={columnRef}
-                        >
-                            <div
-                                className="d-flex gap-1  p-2 align-items-center"
-                                style={{ border: "1px solid blue" }}
-                            >
-                                <div className="mt-1">
-                                    <SearchIcon />
-                                </div>
-                                <input
-                                    type="text"
-                                    placeholder="Search"
-                                    className="p-2 w-100"
-                                    value={searchQuery}
-                                    onChange={(e) => setSearchQuery(e.target.value)}
-                                />
-                            </div>
-
-                            {searchList?.map((columnList: any) => (
-                                <div className="checkboxwithList" key={columnList?.id}>
-                                    <Checkbox
-                                        checked={columnList?.checked}
-                                        onChange={() => handleChecked(columnList?.id)}
-                                    />
-                                    <span>{columnList?.key}</span>
-                                </div>
-                            ))}
-                        </div>
-                    )}
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="">
+          {ShowColumns && (
+            <div
+              className="dashboardcard columnListCard cursorPointer"
+              ref={columnRef}
+            >
+              <div
+                className="d-flex gap-1  p-2 align-items-center"
+                style={{ border: "1px solid blue" }}
+              >
+                <div className="mt-1">
+                  <SearchIcon />
                 </div>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="p-2 w-100"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </div>
 
-                {/* <div className="row justify-content-end">
-         
-        </div> */}
-
-                <div className="row justify-content-between">
-                    <div className="col-12 ">
-                        <div className="d-flex  justify-content-between mt-3 mb-2">
-                            <ul className="d-flex flex-wrap align-items-end gap-2 heading2 textheader cursorPointer p-0 mb-0">
-                                <li
-                                    className="d-flex align-items-center"
-                                    onClick={() => setShowColumns((prev) => !prev)}
-                                >
-                                    <CalendarViewWeekIcon />
-                                    <span className="mx-2 ">Column</span>
-                                </li>
-
-                                <li className="d-flex align-items-center">
-                                    <FilterListIcon />
-                                    <span className="mx-2">Filters</span>
-                                </li>
-                                <li className="d-flex align-items-center">
-                                    <SaveAltIcon />
-                                    <span className="mx-2">Export</span>
-                                </li>
-                                <li className="d-flex align-items-center">
-                                    <HistoryIcon />
-                                    <div className="ms-2">
-                                        <DropdownComponent dropdownlist={year} removepadding={true}/>
-                                    </div>
-                                </li>
-                                <li className="d-flex align-items-center">
-                                    <CalendarMonthIcon />
-                                    <div className="ms-2">
-                                        <DropdownComponent dropdownlist={year}  removepadding={true} />
-                                    </div>
-                                </li>
-                            </ul>
-                            <div className="col-12 col-md-3 ">
-                                    <div className="d-flex gap-1 searchbar ps-2 align-items-center">
-                                        <div className="mt-1">
-                                            <SearchIcon />
-                                        </div>
-                                        <input
-                                            type="text"
-                                            placeholder="Search"
-                                            className="p-2 w-100"
-                                            value={search}
-                                            onChange={(e) => setSearch(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div className="col-12">
-                        <TableWithSort
-                            columns={tableColumns}
-                            rows={tableRows}
-                            dataforicons={false}
-                        />
-                    </div>
+              {searchList?.map((columnList: any) => (
+                <div className="checkboxwithList" key={columnList?.id}>
+                  <Checkbox
+                    checked={columnList?.checked}
+                    onChange={() => handleChecked(columnList?.id)}
+                  />
+                  <span>{columnList?.key}</span>
                 </div>
+              ))}
             </div>
-        </>
-    );
+          )}
+        </div>
+
+        <div className="row justify-content-between">
+          <div className="col-12 ">
+            <div className="d-flex  justify-content-between mt-3 mb-2">
+              <ul className="d-flex flex-wrap align-items-end gap-2 heading2 textheader cursorPointer p-0 mb-0">
+                <li
+                  className="d-flex align-items-center"
+                  onClick={() => setShowColumns((prev) => !prev)}
+                >
+                  <CalendarViewWeekIcon />
+                  <span className="mx-2 ">Column</span>
+                </li>
+
+                <li className="d-flex align-items-center">
+                  <FilterListIcon />
+                  <span className="mx-2">Filters</span>
+                </li>
+                <li className="d-flex align-items-center">
+                  <SaveAltIcon />
+                  <span className="mx-2">Export</span>
+                </li>
+                <li className="d-flex align-items-center">
+                  <HistoryIcon />
+                  <div className="ms-2">
+                    <DropdownComponent
+                      dropdownlist={year}
+                      removepadding={true}
+                    />
+                  </div>
+                </li>
+                <li className="d-flex align-items-center">
+                  <CalendarMonthIcon />
+                  <div className="ms-2">
+                    <DropdownComponent
+                      dropdownlist={year}
+                      removepadding={true}
+                    />
+                  </div>
+                </li>
+              </ul>
+              <div className="col-12 col-md-3 ">
+                <div className="d-flex gap-1 searchbar ps-2 align-items-center">
+                  <div className="mt-1">
+                    <SearchIcon />
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    className="p-2 w-100"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12">
+            <TableWithSort
+              columns={tableColumns}
+              rows={tableRows}
+              dataforicons={false}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Searchwithmenuitems;
