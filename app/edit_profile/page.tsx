8a1ac468b0119d9af697d4a6883b8052b6e46c75
mohamedsafiullah9553 <mohamedsafiullah9553@gmail.com>
2025-02-07 15:58:29 +0530

    hr updated

--- app/edit_profile/page.tsx
+++ app/edit_profile/page.tsx
@@ -12,57 +12,57 @@ import Edit_personal_info from "./component/edit_personal_info";
 import Contact_info from "./component/contact_info";
 
 function Editprofile() {
-  const useColors = Colors(); // Declare useColors once
-  const [selectedTab, setSelectedTab] = useState("personal"); // Default tab
+    const useColors = Colors(); // Declare useColors once
+    const [selectedTab, setSelectedTab] = useState("personal"); // Default tab
 
-  return (
-    <div>
-      <Sidebar>
-        <div className="container-fluid">
-          <BreadcrumbsComponent selectedTab={""} />
-          <div className="row mt-3">
-            <div className="col-12 ">
-              <Link className="unselectcolor para2" href="/profile" passHref>
-                <ArrowBackOutlinedIcon className="me-1" sx={{ fontSize: "25px" }} /> Back
-              </Link>
-            </div>
-            <div className="d-flex mt-4">
-              <p
-                className="para cursorpointer px-3 py-2"
-                onClick={() => setSelectedTab("personal")}
-                style={{
-                  backgroundColor: selectedTab === "personal" ? useColors.themeRed : "transparent",
-                  color: selectedTab === "personal" ? "#fff" : "#6d6777",
-                  borderRadius: "5px",
-                }}
-              >
-                <GroupOutlinedIcon className="me-1" sx={{ fontSize: "18px" }} /> Personal info
-              </p>
-              <p
-                className="para cursorpointer px-3 py-2"
-                onClick={() => setSelectedTab("contact")}
-                style={{
-                  backgroundColor: selectedTab === "contact" ? useColors.themeRed : "transparent",
-                  color: selectedTab === "contact" ? "#fff" : "#6d6777 ",
-                  borderRadius: "5px",
-                }}
-              >
-                <ContactsOutlinedIcon className="me-1" sx={{ fontSize: "18px" }} /> Contact details
-              </p>
-            </div>
-          </div>
+    return (
+        <div>
+            <Sidebar>
+                <div className="container-fluid">
+                    <BreadcrumbsComponent selectedTab={""} />
+                    <div className="row mt-3">
+                        <div className="col-12 ">
+                            <Link className="unselectcolor para2" href="/profile" passHref>
+                                <ArrowBackOutlinedIcon className="me-1" sx={{ fontSize: "25px" }} /> Back
+                            </Link>
+                        </div>
+                        <div className="d-flex mt-4">
+                            <p
+                                className="para cursorpointer px-3 py-2"
+                                onClick={() => setSelectedTab("personal")}
+                                style={{
+                                    backgroundColor: selectedTab === "personal" ? useColors.themeRed : "transparent",
+                                    color: selectedTab === "personal" ? "#fff" : "#6d6777",
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                <GroupOutlinedIcon className="me-1" sx={{ fontSize: "18px" }} /> Personal info
+                            </p>
+                            <p
+                                className="para cursorpointer px-3 py-2"
+                                onClick={() => setSelectedTab("contact")}
+                                style={{
+                                    backgroundColor: selectedTab === "contact" ? useColors.themeRed : "transparent",
+                                    color: selectedTab === "contact" ? "#fff" : "#6d6777 ",
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                <ContactsOutlinedIcon className="me-1" sx={{ fontSize: "18px" }} /> Contact details
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-fluid mb-3">
+                    <div className="row">
+                        <div className="col-12">
+                            {selectedTab === "personal" && <Edit_personal_info />}
+                            {selectedTab === "contact" && <Contact_info />}
+                        </div>
+                    </div>
+                </div>
+            </Sidebar>
         </div>
-        <div className="container-fluid mb-3">
-          <div className="row">
-            <div className="col-12">
-              {selectedTab === "personal" && <Edit_personal_info />}
-              {selectedTab === "contact" && <Contact_info />}
-            </div>
-          </div>
-        </div>
-      </Sidebar>
-    </div>
-  );
+    );
 }
 
 export default Editprofile;
---
+++ app/report/hr/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import DropdownComponent from "@/app/reusableComponent/dropdown";
+import React, { useState } from "react";
+import BreadcrumbsComponent from "@/app/reusableComponent/breadcrumbs";
+import {  hrrepots } from "../../reusableComponent/JsonData";
+import { Colors } from "@/app/reusableComponent/styles";
+import Sidebar from "@/app/sidebar/page";
+
+function Hr_report() {
+  const useColors = Colors(); // Declare useColors once
+  const [selectedTimeOff, setSelectedTimeOff] = useState("");
+  return (
+    <div>
+      <Sidebar>
+        <BreadcrumbsComponent
+          selectedTab={
+            selectedTimeOff === "" ? "Vacation report" : selectedTimeOff
+          }
+        />
+        <div className="row">
+          <div className="col-6">
+            <p className="textheader heading my-2">Hr report</p>
+          </div>
+          <div className="col-6 text-end">
+            <DropdownComponent
+              dropdownlist={hrrepots}
+              color={useColors.themeRed}
+              selectedDatafunction={(data: any) => setSelectedTimeOff(data)}
+            />
+          </div>
+          <div className="">
+            
+          </div>
+        </div>
+      </Sidebar>
+    </div>
+  );
+}
+
+export default Hr_report;
