5421181c104facbb435f7862492242688646c311
mohamedsafiullah9553 <mohamedsafiullah9553@gmail.com>
2025-02-27 17:33:25 +0530

    dashboard hr report updated

--- app/reusableComponent/customization/meetingmodeborder.tsx
+++ app/reusableComponent/customization/meetingmodeborder.tsx
@@ -6,71 +6,92 @@ import { RootState } from "../../redux/store";
 import { useEffect } from "react";
 
 export function BorderColorPicker() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  // Read from Redux store
-  const selectedBorder = useSelector(
-    (state: RootState) => state.meetingmode.background
-  );
+    // Read from Redux store
+    const selectedBorder = useSelector(
+        (state: RootState) => state.meetingmode.background
+    );
 
-  // Default color
-  const defaultColor = "#F4F4F4";
+    // Default color
+    const defaultColor = "transparent";
 
-  // On component mount, set default color if nothing is stored
-  useEffect(() => {
-    const storedColor = localStorage.getItem("meetingModeborder");
-    if (storedColor) {
-      dispatch(setMeetingModeBorderColor({ background: storedColor }));
-    } else {
-      dispatch(setMeetingModeBorderColor({ background: defaultColor }));
-    }
-  }, [dispatch]);
+    // On component mount, set default color if nothing is stored
+    useEffect(() => {
+        const storedColor = localStorage.getItem("meetingModeborder");
+        if (storedColor) {
+            dispatch(setMeetingModeBorderColor({ background: storedColor }));
+        } else {
+            dispatch(setMeetingModeBorderColor({ background: defaultColor }));
+        }
+    }, [dispatch]);
 
-  // Update localStorage when selectedBorder changes
-  useEffect(() => {
-    if (selectedBorder) {
-      localStorage.setItem("meetingModeborder", selectedBorder);
-    }
-  }, [selectedBorder]);
+    // Update localStorage when selectedBorder changes
+    useEffect(() => {
+        if (selectedBorder) {
+            localStorage.setItem("meetingModeborder", selectedBorder);
+        }
+    }, [selectedBorder]);
 
-  // Handle color selection from preset
-  const handlePresetClick = (color: string) => {
-    dispatch(setMeetingModeBorderColor({ background: color }));
-  };
+    // Handle color selection from preset
+    const handlePresetClick = (color: string) => {
+        dispatch(setMeetingModeBorderColor({ background: color }));
+    };
 
-  // Preset colors
-  const colors = ["#f4433626", "#2196f31c", "#ffc10745", "#3f51b53d", "#F4F4F4"];
+    // Preset colors
+    const colors = ["#f4433626", "#2196f31c", "#ffc10745", "#3f51b53d",];
 
-  return (
-    <div className="col-12 mt-3">
-      <h5 className="para mb-0">Meeting Mode Border Color</h5>
-      <div
-        className="d-flex ps-1 mt-2 align-items-center"
-        style={{ gap: "10px" }}
-      >
-        {colors.map((color, index) => (
-          <div
-            key={index}
-            className="colortheme text-center"
-            style={{
-              border: `1px solid ${color}`,
-              borderRadius: "5px",
-            }}
-          >
+    return (
+        <div className="col-12 mt-3">
+            <h5 className="para mb-0">Meeting Mode Border Color</h5>
             <div
-              className="m-1"
-              style={{
-                height: "30px",
-                width: "30px",
-                background: color,
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-              onClick={() => handlePresetClick(color)}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+                className="d-flex ps-1 mt-2 align-items-center"
+                style={{ gap: "10px" }}
+            >
+                {colors.map((color, index) => (
+                    <div
+                        key={index}
+                        className="colortheme text-center"
+                        style={{
+                            border: `1px solid ${color}`,
+                            borderRadius: "5px",
+                        }}
+                    >
+                        <div
+                            className="m-1"
+                            style={{
+                                height: "30px",
+                                width: "30px",
+                                background: color,
+                                borderRadius: "5px",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => handlePresetClick(color)}
+                        />
+                    </div>
+                ))}
+                <div
+
+                    className="colortheme text-center"
+                    style={{
+                        border: `1px solid rgb(244, 244, 244)`,
+                        borderRadius: "5px",
+                    }}
+                >
+                    <div
+                        className="m-1"
+                        style={{
+                            height: "30px",
+                            width: "30px",
+                            background: "rgb(244, 244, 244)",
+                            borderRadius: "5px",
+                            cursor: "pointer",
+                        }}
+                        onClick={() => handlePresetClick("transparent")}
+                    />
+                </div>
+
+            </div>
+        </div>
+    );
 }
